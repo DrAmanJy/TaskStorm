e47f783f2c03b8703ec61031b73a5ef0726c4bf3
Aman Lathar <dramanjy0770@gmail.com>
2025-07-10 20:23:39 +0530

    BUG CHECK

--- front-end/src/services/authServices.js
+++ front-end/src/services/authServices.js
@@ -1,4 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log("ENV:", import.meta.env.VITE_API_BASE_URL, "BASE_URL:", BASE_URL);
 
 export const loginRequest = async (userData) => {
   try {
