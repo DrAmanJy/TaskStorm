9c24ac4301848d14d83b72b26ced9f60a93484a5
Aman Lathar <dramanjy0770@gmail.com>
2025-07-10 20:35:04 +0530

    bug fix

--- front-end/src/services/authServices.js
+++ front-end/src/services/authServices.js
@@ -1,9 +1,8 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-console.log("ENV:", import.meta.env.VITE_API_BASE_URL, "BASE_URL:", BASE_URL);
 
 export const loginRequest = async (userData) => {
   try {
-    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/login`, {
+    const res = await fetch(`${BASE_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(userData),
